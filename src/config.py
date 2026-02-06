055ce1b26ad7f3bee09d9f99156796474d51cc6f
CAgular <52069374+CAgular@users.noreply.github.com>
2026-02-06 11:56:32 +0100

    Add configuration settings for the app

---
+++ src/config.py
@@ -0,0 +1,11 @@
+import os
+
+APP_TITLE = "Knudsen Home App"
+
+DB_PATH = os.path.join("data", "memories.db")
+DB_DRIVE_NAME = "memories.db"
+
+PHOTOS_DIR = "photos"
+PHOTOS_CACHE_DIR = "photos_cache"
+
+ALLOWED_EXTS = [".jpg", ".jpeg", ".png", ".webp"]
